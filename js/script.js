46a4cd21442bc654690b943df557f71d76d3c013
radchenko viacheslav <mibdeveloper@gmail.com>
2017-11-07 19:59:51 +0200

    sign in handler. got it elemnts values.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+'use strict';
+$(function(){
+	$(".signup").click(function () {
+		//get modal content block
+		let $modalContentBlock = $(this).closest(".modal-content");
+		//get elements block
+		let $modalBodyBlock = $modalContentBlock.find(".modal-body");
 
+		let email = $modalBodyBlock.find("[name=email]").val();
+		let password = $modalBodyBlock.find("[name=password]").val();
+		let passwordConfirmation = $modalBodyBlock.find("[name=passwordConfirmation]").val();
+
+		console.log(email);
+		console.log(password);
+		console.log(passwordConfirmation);
+
+	});
+})
