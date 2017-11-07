8932909e46724f221351f7f160da8d3134d4fccb
radchenko viacheslav <mibdeveloper@gmail.com>
2017-11-07 20:45:12 +0200

    post request. Dicaprio

--- js/script.js
+++ js/script.js
@@ -1,49 +1,68 @@
 'use strict';
-$(function(){
+$(function() {
 
-	$('[data-target]').click(function(){
-		let dialogId = $(this).data("target");
-		$(dialogId).find(".alert").hide();
-	})
+    $('[data-target]').click(function() {
+        let dialogId = $(this).data("target");
+        $(dialogId).find(".alert").hide();
+    })
 
-	$(".signup").click(function () {
-		//get modal content block
-		let $modalContentBlock = $(this).closest(".modal-content");
-		//get elements block
-		let $modalBodyBlock = $modalContentBlock.find(".modal-body");
+    $(".signup").click(function() {
+        //get modal content block
+        let $modalContentBlock = $(this).closest(".modal-content");
+        //get elements block
+        let $modalBodyBlock = $modalContentBlock.find(".modal-body");
 
-		//get values
-		let email = $modalBodyBlock.find("[name=email]").val();
-		let password = $modalBodyBlock.find("[name=password]").val();
-		let passwordConfirmation = $modalBodyBlock.find("[name=passwordConfirmation]").val();
+        //get values
+        let email = $modalBodyBlock.find("[name=email]").val();
+        let password = $modalBodyBlock.find("[name=password]").val();
+        let passwordConfirmation = $modalBodyBlock.find("[name=passwordConfirmation]").val();
 
-		let $alertBlock = $modalContentBlock.find(".alert");
-		let errors = [];
-		//check for passwords
-		if(password == ""){
-			errors.push("Password is empty.");
-		}
+        let $alertBlock = $modalContentBlock.find(".alert");
+        let errors = [];
+        //check for passwords
+        if (password == "") {
+            errors.push("Password is empty.");
+        }
 
-		if(password != passwordConfirmation){
-			errors.push("Passwords does not match.");
-		}
-			
-		// TODO: validations 
+        if (password != passwordConfirmation) {
+            errors.push("Passwords does not match.");
+        }
 
+        // TODO: validations 
+        if (errors.length) {
+            showError(errors, $alertBlock);
+        } else {
+            $alertBlock.empty();
+            $alertBlock.hide();
+            // that is ok. let's send the data
 
+            $.post("ajax.php", {
+                'email': email,
+                'password': password,
+            }, requestComplete.bind($alertBlock))
+            .fail(requestFail.bind($alertBlock));
 
-		if(errors.length){
-			$alertBlock.empty();
-			$alertBlock.show();
-			let $ul = $("<ul></ul>");
-			for (var i = 0; i < errors.length; i++) {
-				$ul.append("<li>"+errors[i]+"</li>");
-			}
-			$alertBlock.append($ul);
-		}else{
-			$alertBlock.empty();
-			$alertBlock.hide();
-		}
+        }
 
-	});
+    });
 })
+
+
+function requestComplete(data) {
+
+}
+
+function requestFail(data) {
+	showError([data.status + ":" + data.statusText], this);
+}
+
+
+function showError(errors, alertBlock) {
+    alertBlock.empty();
+    alertBlock.show();
+    let $ul = $("<ul></ul>");
+    for (var i = 0; i < errors.length; i++) {
+        $ul.append("<li>" + errors[i] + "</li>");
+    }
+    alertBlock.append($ul);
+}
